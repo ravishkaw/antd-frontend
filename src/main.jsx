23e9358d8e24a6cb5a26abd331021d57b296bda3
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-12-05 10:51:01 +0530

    remove antdapp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { App as AntdApp } from "antd";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <AntdApp>
-    <App />
-  </AntdApp>
-);
+createRoot(document.getElementById("root")).render(<App />);
